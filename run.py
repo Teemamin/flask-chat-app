7da0f788eca3aa6747f99bc5756c59761d56eea6
Fatima Aminu <teemamin@yahoo.com>
2020-06-10 09:13:01 +0000

    implement basic view and routing

--- run.py
+++ run.py
@@ -6,7 +6,18 @@ app = Flask(__name__)
 @app.route("/")
 
 def index():
-    return "<h1> hello from flask func</h1>"
+    """main page instruction"""
+    return "to send a message use /USERNAME/MESSAGE"
+
+@app.route("/<username>") # remebr that wen written like so "<username>" anything btw that is considered a variable
+
+def user(username):
+    return "Hi " + username
+
+@app.route("/<username>/<message>")
+def send_message(username,message):
+    return "{0}:  {1}" .format(username, message)
+
 
 app.run(host=os.getenv("IP"),port=int(os.getenv("PORT")),debug=True)
 #  We're going to use os.getenv('IP') to get the IP address.
