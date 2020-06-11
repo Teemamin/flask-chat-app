5a4c7e51eae0806dc6c978f37a326f86ce18fa99
Fatima Aminu <teemamin@yahoo.com>
2020-06-11 07:33:54 +0000

    index.html added

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 import os
-from flask import Flask, redirect,render_template
+from flask import Flask, redirect,render_template,request,session 
+# session module is to handle our session variables
 from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = "somerandom123"
+# to generate a session ID we need to give our app a secret key,which is just a random string of letters,num,charctrs
+# in production we'd have it set as enviroment variable, like we did with the IP address
 messages = []
 def add_messages(username,message):
     """takes username ,message and adds it to the list above (messages)""" 
@@ -13,11 +17,18 @@ def get_all_messages():
     """get all messages and separetes them with a br"""
     return "<br>".join(messages)
 
-@app.route("/")
+@app.route("/",methods=["GET","POST"])
 
 def index():
     """main page instruction"""
+    if request.method == "POST" :
+        session["username"] = request.form["username"]
+# if request is POST then we want to create a new variable in our session called username and we want it to be = to username typed from our form input
+    if "username" in session:
+# if username var is set redirect to the content of username session var(take the user to thr page) as routed in the belw ("/<username>")
+        return redirect(session["username"])
     return render_template("index.html")
+
 
 @app.route("/<username>") # remebr that wen written like so "<username>" anything btw that is considered a variable
 
